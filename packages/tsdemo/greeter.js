7e11e55c204d60adb29472f7704d803be9e1fe3f
walkerqiao <150643694@qq.com>
2017-03-29 19:17:20 +0800

    update

---
+++ packages/tsdemo/greeter.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+// var user = "Joe Scott";
+var user = [1, 3, 5];
+document.body.innerHTML = greeter(user);
---
+++ packages/tsdemo/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+// var user = "Joe Scott";
+var user = [1, 3, 5];
+document.body.innerHTML = greeter(user);
